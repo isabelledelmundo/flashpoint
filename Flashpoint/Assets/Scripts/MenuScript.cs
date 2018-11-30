16f3aa0c27f10d5f3ee145a607448f0a157276b3
Isabelle del Mundo <isabelledelmundo@gmail.com>
2018-11-30 01:21:57 -0500

    menu and stuff

---
+++ Flashpoint/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	//main menu buttons
+	public Button createGameButton;
+	public Button loadGameButton;
+	public Button joinGameButton;
+	public Button backButton;
+	public Button exitGameButton;
+
+	
+
+	//scenes
+	public string createGameSceneName;
+	public GameObject activeGamesList;
+	public GameObject saveGamesList;
+
+
+	public void Awake() {
+		//create for all functions w corresponding button
+		createGameButton.onClick.AddListener(CreateGame);
+		loadGameButton.onClick.AddListener(LoadSaveFiles);
+		joinGameButton.onClick.AddListener(OpenActiveGamesList);
+		backButton.onClick.AddListener(CloseActiveGamesList);
+		exitGameButton.onClick.AddListener(ExitGame);
+
+	}
+	public void CreateGame(){
+		SceneManager.LoadScene(createGameSceneName);
+
+	}
+
+	public void LoadSaveFiles(){
+		saveGamesList.SetActive(true);
+	}
+
+	public void OpenActiveGamesList(){
+		activeGamesList.SetActive(true);
+	}
+	public void CloseActiveGamesList(){
+		activeGamesList.SetActive(false);
+	}
+
+
+	public void ExitGame(){
+		Debug.Log("trying to exit game");
+		Application.Quit();
+	}
+
+
+}
--- Flashpoint/Assets/Scripts/SceneTransition.cs
+++ Flashpoint/Assets/Scripts/SceneTransition.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour {
 
-    public void ChangeScene(string sceneName)
-    {
-        Application.LoadLevel(sceneName);
+    public void ChangeScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
     }
+
+
 }
